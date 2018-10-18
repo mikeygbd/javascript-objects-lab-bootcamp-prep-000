bd17546a92645bb18616789a4fd005a9f63ca1a8
Michael Soares <mikey.soares@gmail.com>
2018-10-18 22:12:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,19 @@
 var recipes = {
   pizza: 'cheese',
   soup: 'broth',
-  sandwich:'meat' 
+  sandwich:'meat'
+  function updateObjectWithKeyAndValue(object, key, value) {
+    return Object.assign({}, object, { [key]: value })
+  }
+  function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object
+  }
+  function deleteFromObjectByKey(object, key) {
+    var newObj = Object.assign({}, object)
+    delete newObject[key];
+    return newObject
+  }
+  function destructivelyDeleteFromObjectByKey(object, key) {
+    delete object[key];
+    return object
