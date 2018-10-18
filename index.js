1143f7cc85b2106ab388aad9e30498d707095087
Michael Soares <mikey.soares@gmail.com>
2018-10-18 22:08:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+
+var recipes = {
+  pizza: 'cheese',
+  prop: 1,
+  prop2: 2
+}
+function updateObjectWithKeyAndValue(object, key, value) {
+  var recipes = {
+    prop: 1, prop2: 2
+  }
+  Object.assign({}, 'sauce')
+  return recipes
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+recipes[prop] = 1
+   return recipes
+}
