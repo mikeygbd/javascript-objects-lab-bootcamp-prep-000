5b4eb8f1b2893538c1b4cda40e469f66e3e14eca
Michael Soares <mikey.soares@gmail.com>
2018-10-18 22:12:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,3 @@ var recipes = {
   function updateObjectWithKeyAndValue(object, key, value) {
     return Object.assign({}, object, { [key]: value })
   }
-  function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-    object[key] = value;
-    return object
-  }
-  function deleteFromObjectByKey(object, key) {
-    var newObj = Object.assign({}, object)
-    delete newObject[key];
-    return newObject
-  }
-  function destructivelyDeleteFromObjectByKey(object, key) {
-    delete object[key];
-    return object
